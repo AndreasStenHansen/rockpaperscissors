f8587443219f86807fdf39c7c0ec3439eca0d4fa
Andreas Sten Hansen <andreas_sten_hansen@hotmail.com>
2022-10-31 20:59:31 +0100

    Game function created

--- script.js
+++ script.js
@@ -9,25 +9,60 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let playerInput = prompt("Please provide choice")
+    const playerInput = prompt("Please provide choice")
     return playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase()
 }
 
 function SingleRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return "Draw!"
+        console.log("Draw!")
+        return
     } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        return "You Win! Rock beats Scissors"
+        console.log("You Win! Rock beats Scissors")
+        return "Player win"
     } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-        return "You Lose! Paper beats Rock"
+        console.log("You Lose! Paper beats Rock")
+        return "Player lose"
     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-        return "You Lose! Scissors beats Paper"
+        console.log("You Lose! Scissors beats Paper")
+        return "Player lose"
     } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-        return "You Win! Paper Beats Rock"
+        console.log("You Win! Paper Beats Rock")
+        return "Player win"
     } else if (playerSelection == "Scissors" && computerSelection == "Rocks") {
-        return "You Lose! Rocks Beats Scissors"
+        console.log("You Lose! Rocks Beats Scissors")
+        return "Player lose"
     } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        return "You Win! Scissors Beats Paper"
+        console.log("You Win! Scissors Beats Paper")
+        return "Player win"
+    }   
+}
+
+function game() {
+    playRound();
+}
+
+function playRound() {
+    player = 0;
+    computer = 0;
+    for (let i = 0; i < 5; i++) {
+        const computerChoice = getComputerChoice();
+        const playerChoice = getPlayerChoice();
+        const winner = SingleRound(playerChoice, computerChoice);
+        if (winner == "Player win") {
+            player++;
+        } else if (winner == "Player lose") {
+            computer++;
+        } else {
+
+        }
     }
-    
+    if (player > computer) {
+        console.log("The player wins in total")
+    } else if (computer > player) {
+        console.log("The computer wins in total")
+    } else if (computer == player) {
+        console.log("Draw between player and computer in total")
+    }
+
 }
