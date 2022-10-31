77d71c40af7df67f8ead266644c5e48ba9ad21f3
Andreas Sten Hansen <andreas_sten_hansen@hotmail.com>
2022-10-31 20:35:43 +0100

    SingleRound algorithm created

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    return prompt("Please provide choice")
+    let playerInput = prompt("Please provide choice")
+    return playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase()
+}
+
+function SingleRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Draw!"
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You Win! Paper Beats Rock"
+    } else if (playerSelection == "Scissors" && computerSelection == "Rocks") {
+        return "You Lose! Rocks Beats Scissors"
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You Win! Scissors Beats Paper"
+    }
+    
 }
