29de8079a235fe3ea2aa633337310728a131e82a
Andreas Sten Hansen <andreas_sten_hansen@hotmail.com>
2022-11-09 21:54:44 +0100

    Added interactivitiy to the page

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// function getComputerChoice() {
-//     var computerChoices = [
-//         'Rock',
-//         'Paper',
-//         'Scissors'
-//     ]
+function getComputerChoice() {
+    var computerChoices = [
+        'Rock',
+        'Paper',
+        'Scissors'
+    ]
 
-//     return computerChoices[Math.floor(Math.random() * computerChoices.length)]
-// }
+    return computerChoices[Math.floor(Math.random() * computerChoices.length)]
+}
 
 // function getPlayerChoice() {
 //     const playerInput = prompt("Please provide choice")
@@ -67,12 +67,66 @@
 
 // }
 
-// Adding interactivity to the page
+
+// Drop shadow to the first three icons that the user can pick between
 
 var humanRock = document.getElementById('rock');
-//humanRock.mouseover.style.border = 'solid 2px #CCC'
 
 humanRock.addEventListener('mouseover', function(){
-    console.log("Hello");
-    this.style.border = '2px solid #CCC'
+    this.style.filter = "drop-shadow(19px 2px 10px #B88A30)";
+})
+
+humanRock.addEventListener('mouseout', function(){
+    this.style.filter = "";
+})
+
+var humanPaper = document.getElementById('paper');
+
+humanPaper.addEventListener('mouseover', function(){
+    this.style.filter = "drop-shadow(19px 2px 10px #B88A30)";
+})
+
+humanPaper.addEventListener('mouseout', function(){
+    this.style.filter = "";
+})
+
+var humanScissors = document.getElementById('scissors');
+
+humanScissors.addEventListener('mouseover', function(){
+    this.style.filter = "drop-shadow(19px 2px 10px #B88A30)";
+})
+
+humanScissors.addEventListener('mouseout', function(){
+    this.style.filter = "";
+})
+
+// Function for updating the computers pick
+
+function showComputerChoice() {
+    var computerChoice = getComputerChoice();
+    //console.log(computerChoice);
+    var computer = document.getElementById('computerPicture');
+    //console.log(computer);
+    if  (computerChoice == "Rock") {
+        computer.src = "./png/rock.png"
+    }
+    else if  (computerChoice == "Paper") {
+        computer.src = "./png/paper-stack.png"
+    }
+    else if  (computerChoice == "Scissors") {
+        computer.src = "./png/scissor.png"
+    }
+}
+
+// Event listener for clicks on the icons
+humanPaper.addEventListener('click', function(){
+    showComputerChoice();
+})
+
+humanRock.addEventListener('click', function(){
+    showComputerChoice();
+})
+
+humanScissors.addEventListener('click', function(){
+    showComputerChoice();
 })
